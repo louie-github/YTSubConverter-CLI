d842317b27e8b591ad78914f4dc50ddb6aece9ce
louie-github <30176969+louie-github@users.noreply.github.com>
2021-05-23 15:27:54 +0800

    Add some machine translation to comments

--- YTSubConverter.CLI/Properties/AssemblyInfo.cs
+++ YTSubConverter.CLI/Properties/AssemblyInfo.cs
@@ -6,6 +6,10 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
+// DeepL Translation:
+// General information about an assembly is controlled through the
+// following set of attributes. To change the information associated
+// with an assembly, modify the values of these attributes.
 [assembly: AssemblyTitle("YTSubConverter-CLI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -18,9 +22,16 @@ using System.Runtime.InteropServices;
 // ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
 // 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
 // その型の ComVisible 属性を true に設定してください。
+// DeepL translation:
+// If ComVisible is set to false, the types in this assembly cannot be
+// referenced by COM components. If you need to access a type in this
+// assembly from COM, set the ComVisible attribute of that type to
+// true.
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+// DeepL translation:
+// If this project is published to COM, the following GUID will be the ID of the typelib
 [assembly: Guid("fe339cad-14bd-43db-a700-5ddc8b75f43b")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
@@ -32,6 +43,18 @@ using System.Runtime.InteropServices;
 //
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+//
+// Google Translate translation:
+// Assembly version information consists of four values:
+//
+//      Major version
+//      Minor version
+//      Build number
+//      Revision
+//
+// You can specify all values or use the following to set the build and
+// revision numbers to defaults:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.2")]
 [assembly: AssemblyFileVersion("1.5.2")]
